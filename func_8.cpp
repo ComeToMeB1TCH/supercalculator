57e5e19e777c4190e07136de64259868ea5df613
ComeToMeB1TCH <92302987+ComeToMeB1TCH@users.noreply.github.com>
2022-04-12 13:38:57 +0300

    Update func_8.cpp

--- func_8.cpp
+++ func_8.cpp
@@ -17,23 +17,23 @@ void SorticMultiplication2(string first, string second) {
 void SorticPluse2(string first, string second) {
     if (Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))) > second) {
         if (Kol_minuseDoNumbers(first) % 2 == 0 && Kol_minuse(Reverse(first)) % 2 != 0)
-            cout << ProverkaMinus(second, Reverse(no_pluse(no_minuse(Reverse(no_pluse(no_minuse(first)))))));
+            return ProverkaMinus(second, Reverse(no_pluse(no_minuse(Reverse(no_pluse(no_minuse(first)))))));
         if (Kol_minuseDoNumbers(first) % 2 != 0 && Kol_minuse(Reverse(first)) % 2 == 0)
-            cout << "-" << ProverkaMinus(Reverse(no_pluse(no_minuse(Reverse(no_pluse(no_minuse(first)))))), second);
+            return pluseminus(1) + ProverkaMinus(Reverse(no_pluse(no_minuse(Reverse(no_pluse(no_minuse(first)))))), second);
         if (Kol_minuseDoNumbers(first) % 2 != 0 && Kol_minuse(Reverse(first)) % 2 != 0)
-            cout << "-" << ProverkaPluse(Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))), second);
+            return pluseminus(1) + ProverkaPluse(Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))), second);
         if (Kol_minuseDoNumbers(first) % 2 == 0 && Kol_minuse(Reverse(first)) % 2 == 0)
-            cout << ProverkaPluse(Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))), second);
+            return ProverkaPluse(Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))), second);
     }
     else {
         if (Kol_minuseDoNumbers(first) % 2 == 0 && Kol_minuse(Reverse(first)) % 2 != 0)
-            cout << "-" << ProverkaMinus(second, Reverse(no_pluse(no_minuse(Reverse(no_pluse(no_minuse(first)))))));
+            return pluseminuse(1) + ProverkaMinus(second, Reverse(no_pluse(no_minuse(Reverse(no_pluse(no_minuse(first)))))));
         if (Kol_minuseDoNumbers(first) % 2 != 0 && Kol_minuse(Reverse(first)) % 2 == 0)
-            cout << ProverkaMinus(second, Reverse(no_pluse(no_minuse(Reverse(no_pluse(no_minuse(first)))))));
+            return ProverkaMinus(second, Reverse(no_pluse(no_minuse(Reverse(no_pluse(no_minuse(first)))))));
         if (Kol_minuseDoNumbers(first) % 2 != 0 && Kol_minuse(Reverse(first)) % 2 != 0)
-            cout << "-" << ProverkaPluse(Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))), second);
+            return pluseminus(1) + ProverkaPluse(Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))), second);
         if (Kol_minuseDoNumbers(first) % 2 == 0 && Kol_minuse(Reverse(first)) % 2 == 0)
-            cout << ProverkaPluse(Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))), second);
+            return ProverkaPluse(Reverse(no_pluse(no_minuse(Reverse(no_minuse(first))))), second);
     }
 }
 
@@ -54,6 +54,14 @@ bool Proverka(string enter) {
         return true;
 }
 
+string pluseminus(int len) {
+    string resultat;
+    for (len; len >= 0; len--) {
+        resultat += "-";
+    }
+    return resultat;
+}
+
 void Menu() {
     cout << "Enter:Firts number + or - or * and second number !everything is merged" << endl;
     string enter, operation, first, second;
@@ -64,8 +72,12 @@ void Menu() {
         second = no_pluse(Second_Num(enter));
         string len = second + first;
         string lenNoMinuse = second + no_minuse(first);
-        if (Reverse(no_minuse(Reverse(first)))[Len(first) - 1 - (Kol_minuse(Reverse(first)) - 1)] == '+')
-            SorticPluse2(first, second);
+        if (Reverse(no_minuse(Reverse(first)))[Len(first) - 1 - (Kol_minuse(Reverse(first)) - 1)] == '+'){
+            if(SorticPluse2(first, second)[0] == '-' && SorticPluse2(first, second)[1] == '0')
+                cout << "0";
+            else
+                cout << SorticPluse2(first, second);
+        }
         if (Reverse(no_minuse(Reverse(first)))[Len(first) - 1 - (Kol_minuse(Reverse(first)) - 1)] == '*')
             SorticMultiplication2(first, second);
         if (OperationZnak(enter) == '+')
